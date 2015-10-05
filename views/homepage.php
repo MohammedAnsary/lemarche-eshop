f0170b152ee139a24e1ee886fe8165f203cefd6d
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2015-10-05 23:43:10 +0200

    Products added

--- views/homepage.php
+++ views/homepage.php
@@ -1,5 +1,151 @@
 <?php
 	$title = 'Le Marché';
 	require_once('header.php');
+?>
+
+		<div class="product-wrapper">
+			<div class="product">
+				<img src="http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10914342_331240250416001_40184211_a.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="https://pbs.twimg.com/profile_images/602843787623145474/NLWOA8Y4.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://static.comicvine.com/uploads/original/8/89742/1659763-wah2.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://vignette2.wikia.nocookie.net/hunterxhunter/images/c/cd/Kurapika_close_up.png/revision/latest?cb=20120416113110">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://bdn.i.ntere.st/p/11478953/image">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10914342_331240250416001_40184211_a.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="https://pbs.twimg.com/profile_images/602843787623145474/NLWOA8Y4.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://static.comicvine.com/uploads/original/8/89742/1659763-wah2.jpg">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://vignette2.wikia.nocookie.net/hunterxhunter/images/c/cd/Kurapika_close_up.png/revision/latest?cb=20120416113110">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+			<div class="product">
+				<img src="http://bdn.i.ntere.st/p/11478953/image">
+				<div class="product-details">
+					<div class="product-name">
+						Product
+					</div>
+					<div class="product-price">
+						$20.0
+					</div>
+				</div>
+				<div class="add-to-cart">
+					Add to cart
+				</div>
+			</div>
+		</div>
+
+<?php
 	require_once('footer.php');
 ?>
